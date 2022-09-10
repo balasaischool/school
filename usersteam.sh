19e0f1736cda27a69380ed29f1186c8d6c816dc0
kiranmai nayakanti <107303997+kiranmai2323@users.noreply.github.com>
2022-09-10 10:04:46 +0530

    Delete usersteam.sh

--- usersteam.sh
+++
@@ -1,6 +0,0 @@
-echo "adding users to the team"
-curl \
-        -X GET \
--u sukesh1231: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 \
-https://github.com/orgs/balasaischool/teams/chukkii/members/kiranmai2323 \
--d '{"role"="write","email":lakshmikiranmai24@gmail.com,homepage:"www.github.com"}'
