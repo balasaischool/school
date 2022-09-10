013e70554ba5094450f1e2255da6eff7854f0f29
kiranmai2323 <lakshmikiranmai24@gmail.com>
2022-09-10 09:18:43 +0530

    maven-web-application/

---
+++ usersteam.sh
@@ -0,0 +1,6 @@
+echo "adding users to the team"
+curl \
+        -X GET \
+-u sukesh1231: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 \
+https://github.com/orgs/balasaischool/teams/chukkii/members/kiranmai2323 \
+-d '{"role"="write","email":lakshmikiranmai24@gmail.com,homepage:"www.github.com"}'
