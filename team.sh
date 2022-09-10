7f7109e3e74b79023a8b76955609e0f36e8fb001
kiranmai nayakanti <107303997+kiranmai2323@users.noreply.github.com>
2022-09-10 10:04:24 +0530

    Delete team.sh

--- team.sh
+++
@@ -1,9 +0,0 @@
-echo "Creating the team members."
-
-
-curl \
-  -X POST \
-  -u sukesh1231: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 \
-https://api.github.com/orgs/balasaischool/teams \
-  -d '{"name":"sukkii","description":"This is my team","homepage":"https://github.com","public":true,"has_issues":true,has_wiki":true}'
-
